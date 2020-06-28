ca445bed683ed0b42b77c08012e7ecfea9966788
MangoStick <openhouseparty28@gmail.com>
2020-06-29 03:24:57 +0700

    initial

---
+++ lib/constants/strings.dart
@@ -0,0 +1,21 @@
+class Strings {
+  Strings._();
+
+  static const String about_me = 'About Me';
+  static const String about = 'About';
+  static const String me = ' Me';
+  static const String portfoli = 'Portfoli';
+  static const String o = 'o';
+  static const String headline = 'I\'am Jakkapat Noimhor, Web & Mobile App Developer from Chiang Rai, Thailand';
+  static const String summary = 'coming soon..';
+  static const String experience = 'Experience';
+  static const String exp_detail = 'coming soon..';
+  static const String skills_i_have = 'What Skill I Have';
+  static const String rights_reserved = '©2018-Now.';
+
+  // menu items
+  static const String menu_home = 'Home';
+  static const String menu_about = 'About';
+  static const String menu_contact = 'Contact';
+
+}
